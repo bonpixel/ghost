cbf3a8215a3eef6571c057a2c6530fa68948dbbf
Bryan Nichols <nicholsbryan@gmail.com>
2014-02-21 22:40:18 -0800

    fixing the url

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ config = {
     // ### Development **(default)**
     development: {
         // The url to use when providing links to the site, E.g. in RSS and email.
-        url: 'http://droplet.bonpixel.com',
+        url: 'http://blog.bonpixel.com',
 
         // Example mail config
         // Visit http://docs.ghost.org/mail for instructions
@@ -44,7 +44,7 @@ config = {
     // When running Ghost in the wild, use the production environment
     // Configure your URL and mail settings here
     production: {
-        url: 'http://droplet.bonpixel.com',
+        url: 'http://blog.bonpixel.com',
         mail: {},
         database: {
             client: 'sqlite3',
